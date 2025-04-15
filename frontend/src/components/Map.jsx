9f91c08c3db51096ddead90de9174dc6db93578c
Jack855558 <sportingjdh@icloud.com>
2025-04-15 09:51:00 -0500

    test in console

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -11,11 +11,14 @@ const Map = () => {
     //         .catch((error) => console.error('Error fetching map:', error));
     // }, []);
 
+    console.log('Axios default base URL:', axios.defaults.baseURL);
+
     useEffect(() => {
         axios.get('https://Wichita-Data-1.onrender.com/test')
             .then((response) => setMapHTML(response.data))
             .catch((error) => console.error('Error fetching map:', error));
     }, []);
+
 
 
     //return <div dangerouslySetInnerHTML={{ __html: mapHTML }} />
