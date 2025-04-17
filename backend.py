104becf0a60b2e5f42ea5dd5c700b5552e4ed603
Jack855558 <sportingjdh@icloud.com>
2025-04-17 09:48:29 -0500

    Fix backend error 3

--- backend.py
+++ backend.py
@@ -65,4 +65,5 @@ def check_locations():
         }), 400
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
